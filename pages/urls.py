7ec82e6542b07499daf604eef252dc800f57929e
SunFrogThe <turtaleta@gmail.com>
2019-02-17 15:09:15 +0200

    add about page

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home')
+    path('about/', views.AboutPage.as_view(), name='about'),
+    path('', views.HomePage.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
     template_name = 'pages/home.html'
+
+
+class AboutPage(TemplateView):
+    template_name = 'pages/about.html'
